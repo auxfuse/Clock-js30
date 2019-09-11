f0edf770cdc066f3a926691fab26724ff92bbb32
auxfuse <Anthony.obrien2@hse.ie>
2019-09-11 15:27:27 +0100

    All hands on clock moving

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,9 +3,9 @@
 Global Variables
 ==================
 */
-const secondHand = document.querySelector(".second-hand");
-const minHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+const secondsHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
 
 
 /*
@@ -14,18 +14,29 @@ Main Functions
 ==================
 */
 
-    // setDate function to get time and split to hours/minutes/seconds and rotate each hand as needed.
-    function setDate() {
-        // Create and retrieve Current Date and store in variable.
-        const now = new Date();
+// setDate function to get time and split to hours/minutes/seconds and rotate each hand as needed.
+function setDate() {
+    // Create and retrieve Current Date and store in variable.
+    const now = new Date();
 
-        // Retrieve seconds from current Date variable "now".
-        const seconds = now.getSeconds();
-        // Convert seconds into notable points in 360degrees and use style.transform to implement into css for seconds hand tick movement.
-        const secondsDegrees = ((seconds / 60) * 360) + 90;
-        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    // Retrieve seconds from current Date variable "now".
+    const seconds = now.getSeconds();
+    // Convert seconds into notable points in 360degrees and use style.transform to implement into css for seconds hand tick movement.
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    }
+    // Retrieve minutes from current Date variable "now".
+    const mins = now.getMinutes();
+    // Convert minute into notable points in 360degrees and use style.transform to implement into css for minutes hand tick movement.
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    // Retrieve hours from current Date variable "now".
+    const hours = now.getHours();
+    // Convert hour into notable points in 360degrees and use style.transform to implement into css for hours hand tick movement.
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
 
 /*
 ==================
@@ -35,3 +46,5 @@ Assistant Functions
 
 // Use setInterval built in JS function to update setDate function every 1 second.
 setInterval(setDate, 1000);
+
+setDate();
